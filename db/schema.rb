4a8d9363d4721c3b96c8c87224ed084c2095c496
Le Van Tho <lvtho@gmail.com>
2016-12-14 11:12:46 +0700

    change model user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161207024913) do
+ActiveRecord::Schema.define(version: 20161214040727) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "action_id"
@@ -110,10 +110,10 @@ ActiveRecord::Schema.define(version: 20161207024913) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
-    t.string   "password"
     t.boolean  "is_admin"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
