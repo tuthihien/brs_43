ef238b427ff4b438b2ef61a08fbe598c4073df4f
Le Van Tho <lvtho@gmail.com>
2016-12-21 23:36:09 +0700

    fix users

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
-  def gravatar_for user, options = {Settings.size_gravatar}
+  def gravatar_for user, options = Settings.size_gravatar
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    size = options[:size]
+    size = options[Settings.size_gravatar]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag gravatar_url, alt: user.username, class: "gravatar"
   end
