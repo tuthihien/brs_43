5c2e277a820b6deb04f1b720ca6ffe120ef04871
quoc viet <quocvietlee@gmail.com>
2016-12-23 09:23:02 +0700

    gadmin-book

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,14 @@ class Book < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :rates, dependent: :destroy
   has_many :requests, dependent: :destroy
+  mount_uploader :image, ImageUploader
+
+  scope :order_alphabet, -> {order title: :ASC}
+
+  private
+  def image_size
+    if image.size > Settings.size_image.megabytes
+      errors.add :image, t("error_image")
+    end
+  end
 end
